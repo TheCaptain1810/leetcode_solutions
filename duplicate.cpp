9b7f5c61a7bf51442ff9f3b00d87d017c94cc98e
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-11-24 11:36:38 +0530

    20241124

--- duplicate.cpp
+++ duplicate.cpp
@@ -1,8 +1,11 @@
-class Solution {
+class Solution
+{
 public:
-    bool containsDuplicate(vector<int>& nums) {
+    bool containsDuplicate(vector<int> &nums)
+    {
         unordered_map<int, int> seen;
-        for (int num : nums) {
+        for (int num : nums)
+        {
             if (seen[num] >= 1)
                 return true;
             seen[num]++;
