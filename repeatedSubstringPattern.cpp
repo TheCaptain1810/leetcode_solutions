651452ab4c96a675ed55fb6ad757cad34d1b86ec
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2025-01-09 19:55:47 +0530

    20250109(3)

---
+++ repeatedSubstringPattern.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    bool repeatedSubstringPattern(string s)
+    {
+        string t = (s + s);
+        t = t.substr(1, t.size() - 2);
+        return t.find(s) != string::npos;
+    }
+};
