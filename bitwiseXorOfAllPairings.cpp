00eab923e16d2bf065e245bc63b96d923cf4e0eb
Pranav Pise <itspranavpise@gmail.com>
2025-01-16 10:04:16 +0530

    20250116

---
+++ bitwiseXorOfAllPairings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int xor1 = 0;
+        int xor2 = 0;
+
+        if (nums1.size() % 2) {
+            for (int num: nums2) {
+                xor2 ^= num;
+            }
+        }
+
+        if (nums2.size() % 2) {
+            for (int num: nums1) {
+                xor1 ^= num;
+            }
+        }
+
+        return xor1 ^ xor2;
+    }
+};
